110c9958fa20b83fa2ab346a73437b58afcc77d2
Nursel Demirkiran <demirkiran3@gmail.com>
2023-12-21 23:38:31 +0300

    ToDo App

--- src/App.js
+++ src/App.js
@@ -1,2 +1,78 @@
 
+import './App.css';
+import { useState } from 'react';
+
+const TodoInput = ({ todo, setTodo, addTodo }) => {
+  return (
+    <div className="input-wrapper">
+      <input
+        type="text"
+        name="todo"
+        value={todo}
+        placeholder="Create a new todo"
+        onChange={(e) => {
+          setTodo(e.target.value);
+        }}
+      />
+      <button className="add-button" onClick={addTodo}>
+        Add
+      </button>
+    </div>
+  );
+};
+
+const TodoList = ({ list, remove }) => {
+  return (
+    <>
+      {list?.length > 0 ? (
+        <ul className="todo-list">
+          {list.map((entry, index) => (
+            <div className="todo" key={index}>
+              <li>{entry}</li>
+              <button
+                className="delete-button"
+                onClick={() => {
+                  remove(entry);
+                }}
+              >
+                Delete
+              </button>
+            </div>
+          ))}
+        </ul>
+      ) : (
+        <div className="empty">
+          <p>No task found</p>
+        </div>
+      )}
+    </>
+  );
+};
+
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState('');
+
+  const addTodo = () => {
+    if (todo !== '') {
+      setTodos([...todos, todo]);
+      setTodo('');
+    }
+  };
+
+  const deleteTodo = (text) => {
+    const newTodos = todos.filter((todo) => todo !== text);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="App">
+      <h1>Todo List</h1>
+      <TodoInput todo={todo} setTodo={setTodo} addTodo={addTodo} />
+      <TodoList list={todos} remove={deleteTodo} />
+    </div>
+  );
+}
+
+export default App;
 
